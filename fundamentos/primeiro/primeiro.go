9768061d30aafa5cf581e34e080945dbf438d1ba
Mauricio Rocha <mauricio@MacBook-Pro-de-Mauricio.local>
2021-01-14 23:11:52 -0300

    initial commit

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+//Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em GO são organizados em pacotes
+e para usá-los é necessário declará-los em vários imports
+*/
+import "fmt"
+
+//A função main é a porta de entrada de um programa GO
+func main() {
+	fmt.Print("Primeiro: ")
+	fmt.Print("Programa")
+}
